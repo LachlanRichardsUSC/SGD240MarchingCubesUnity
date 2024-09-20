b93dcaa89247056a5a496e656b06cde34257e1d9
LachlanRichardsUSC <ljr044@student.usc.edu.au>
2024-09-20 18:25:04 +1000

    Laying framework for Compute Shader
    Added a couple of helper classes to the project, WIP

---
+++ Assets/MarchingCubes/Scripts/Channel.cs
@@ -0,0 +1,12 @@
+namespace ComputeShaderUtility
+{
+    // Enum to represent different colour channels for use in a Compute Shader
+    public enum Channel
+    {
+        Red,
+        Green,
+        Blue,
+        Alpha,
+        Zero
+    }
+}
